f8a0262af24ff36f16900da2bc5b1a8d9d708f8d
Raghuram <1993raghuram@gmail.com>
2020-05-07 14:22:48 +0530

    Added bookly

---
+++ assignments/5 - bookly/app.js
@@ -0,0 +1,39 @@
+const addBookDOM  = document.querySelector("#add-book");
+const bookInputDOM = document.querySelector(".book-input");
+const ulDOM = document.querySelector("#book-list ul");
+const hideBooksDOM = document.querySelector("#hide");
+const searchBooksDOM = document.querySelector("#search-books");
+const searchBooksInputDOM = document.querySelector("#search-books input");
+
+const allBooks = [];
+
+function createUI(books = allBooks){
+    ulDOM.innerHTML = "";
+    books.forEach(book => {
+        ulDOM.innerHTML += `
+            <li>
+                ${book}
+            </li>
+        `
+    });
+    bookInputDOM.value = "";
+}
+
+addBookDOM.addEventListener("submit",function(event){
+    event.preventDefault();
+    let bookName = bookInputDOM.value.trim().toLowerCase();
+    allBooks.push(bookName);
+    createUI();
+})
+
+hideBooksDOM.addEventListener("click",function(event){
+    ulDOM.classList.toggle("toggle-display");
+})
+
+searchBooksDOM.addEventListener("submit",function(event){
+    event.preventDefault()
+    const bookName = searchBooksInputDOM.value.trim().toLowerCase();
+    
+    let searchedBooks = allBooks.filter(book => book.includes(bookName));
+    createUI(searchedBooks);
+})
